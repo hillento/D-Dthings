e5545436ef8ed322a7cd00b67f4818fc4fae47ea
Alex Brown <42818319+hillento@users.noreply.github.com>
2019-03-03 23:50:55 -0700

    Update Rollers.cs
    Added the basic auto roller. Rolls 4 dice minus the lowest dice and returns an array of 6 values

--- CharacterCreator/CharacterCreator/Rollers.cs
+++ CharacterCreator/CharacterCreator/Rollers.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace CharacterCreator
 {
-  class Rollers
+  public class Rollers
   {
+    Random rnd = new Random();
+    private int[] rolledStats = new int[6];
+
+
+    private int[] AutoRoller()
+    {
+      for(int m=0; m<6; m++)
+      {
+        int min = 100;
+        int[] initialroll = { 0, 0, 0, 0 };
+        for(int n=0; n<4; n++)
+        {
+          initialroll[n] = rnd.Next(1, 7);
+          if (min > rolledStats[n])
+          {
+            min = rolledStats[n];
+          }
+        }
+        rolledStats[m] = rolledStats[0] + rolledStats[1] + rolledStats[2] + rolledStats[3] - min;
+      }
+      return rolledStats;
+    }
   }
+
 }
